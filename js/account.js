74b1a3222ccc03829b24114b5f7f7fb8dc9c5764
Flober57 <florent.bernar@outlook.fr>
2023-12-04 16:55:10 +0100

    add

--- js/account.js
+++ js/account.js
@@ -1,16 +1,16 @@
 function showTab(tabId) {
-    // Masquer tous les contenus
-    document.querySelectorAll('.tab__content').forEach(function (tabContent) {
-      tabContent.style.display = 'none';
-      tabContent.classList.remove('active-tab'); // Supprimer la classe active-tab
-    });
-  
-    // Afficher le contenu de l'onglet actuel si l'ID existe
-    var currentTab = document.getElementById(tabId);
-    if (currentTab) {
-      currentTab.style.display = 'block';
-      currentTab.classList.add('active-tab'); // Ajouter la classe active-tab
-    } else {
-      console.error('L\'élément avec l\'ID ' + tabId + ' n\'existe pas.');
-    }
+  // Masquer tous les contenus
+  document.querySelectorAll('.tab__content').forEach(function (tabContent) {
+    tabContent.style.display = 'none';
+    tabContent.classList.remove('active-tab'); // Supprimer la classe active-tab
+  });
+
+  // Afficher le contenu de l'onglet actuel si l'ID existe
+  var currentTab = document.getElementById(tabId);
+  if (currentTab) {
+    currentTab.style.display = 'block';
+    currentTab.classList.add('active-tab'); // Ajouter la classe active-tab
+  } else {
+    console.error('L\'élément avec l\'ID ' + tabId + ' n\'existe pas.');
   }
+}
