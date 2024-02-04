6b64f5cb38f2567e34588e10a42a92f68f1e4c31
Tardice <florent.bernar@outlook.fr>
2024-02-04 21:39:39 +0100

    Update logout.php

--- php/logout.php
+++ php/logout.php
@@ -1,6 +1,22 @@
 <?php
 session_start();
  
+if ($_SESSION["user_role_ID"] == 3) {
+    $delete_sql = "DELETE FROM user WHERE user_ID = ?";
+    if ($delete_stmt = mysqli_prepare($link, $delete_sql)) {
+        mysqli_stmt_bind_param($delete_stmt, "i", $param_id_delete);
+        $param_id_delete = $_SESSION["user_ID"];
+        if (mysqli_stmt_execute($delete_stmt)) {
+            session_destroy();
+            header("location: baldflix_login.php");
+            exit();
+        } else {
+            echo "Erreur lors de la suppression du compte.";
+        }
+        mysqli_stmt_close($delete_stmt);
+    }
+  }
+
 $_SESSION = array();
 session_destroy();
  
